2db1daa0a6667c93bf3d4242cb70d2b7bf38e42a
Rahel Wilking <rwilking@brockhaus-ag.de>
2026-06-02 09:07:34 +0000

    schaden hub und schadensumme satellit

---
+++ models/raw_vault/sat_schaden_schadensumme.sql
@@ -0,0 +1,27 @@
+{{ config(unique_key=['hk_schaden', 'load_ts']) }}
+
+with src as (
+    select distinct
+        {{ get_hash_key(['schaden_id']) }} as hk_schaden,
+
+        sum(schaden_summe_eur) over (partition by schaden_id) as schaden_summe_gesamt_eur,
+        load_ts,
+        'stg_schaden_events' as record_source
+    from {{ ref('stg_schaden_events') }}
+    where event_typ = 'MELDUNG' 
+),
+src2 as (
+    select *,
+        {{ get_hash_diff([
+            'schaden_summe_gesamt_eur'
+        ]) }} as hd_schaden_schadensumme,
+    from src
+)
+
+select * from src2
+{% if is_incremental() %}
+-- nur neue inhaltliche Stände einfügen
+where concat(hk_schaden, hd_schaden_schadensumme) not in (
+    select concat(hk_schaden, hd_schaden_schadensumme) from {{ this }}
+)
+{% endif %}
