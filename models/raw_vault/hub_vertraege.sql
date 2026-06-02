a0abb9377c6bf3982f24f32b1cc71eef52c06b64
Morris Emming <memming@brockhaus-ag.de>
2026-06-02 09:16:43 +0000

    macro Optimierung

--- models/raw_vault/hub_vertraege.sql
+++ models/raw_vault/hub_vertraege.sql
@@ -1,16 +1,1 @@
-{{ config(unique_key='hk_vertraege') }}
-
-with src as (
-    select
-        {{ get_hash_key(['vertrag_id']) }}  as hk_vertraege,
-        vertrag_id,
-        load_ts,
-        'stg_vertraege' as record_source
-    from {{ ref('stg_vertraege') }}
-    where vertrag_id is not null
-)
-
-select * from src
-{% if is_incremental() %}
-where hk_vertraege not in (select hk_vertraege from {{ this }})
-{% endif %}
+{{ macro_hub('vertraege', 'stg_vertraege', 'vertrag_id') }}
